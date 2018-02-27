5fb980521bee49f5734687af0983d3bee2de0acd
Palaniappan <palaniappan.feb22@gmail.com>
2018-02-27 17:46:25 -0600

    Add few features

--- location.py
+++ location.py
@@ -1,21 +1,57 @@
 import nltk
 from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import RandomForestClassifier
 import pdb
+import re
+import os
+
+
+def get_previous_word(word, line):
+    index = line.index(word)
+    if index == 0:
+        return None
+
+    words_before = line[:index-1].split(" ")
+    return words_before[-1]
+
+
+def get_next_word(word, line):
+    index = line.index(word) + len(word) + 1
+    return line[index:].split(" ")[0]
+
+
+def is_all_caps(word):
+    return 1 if word.isupper else 0
 
 
 def is_first_char_capitalised(word):
-    return 1 if len(word) > 1 and word[0].isupper() else 0
+    words = word.split(" ")
+    for w in words:
+        if w[0].islower():
+            return 0
+
+    return 1
+
+
+def previous_word_direction(word, word_context):
+    prefixes = ('north', 'east', 'west', 'south',)
+    previous_word = get_previous_word(word, word_context)
+
+    if not previous_word:
+        return 0
+
+    return 1 if previous_word.lower().startswith(prefixes) else 0
 
 
 def has_keywords_before(word, word_context):
     keywords = ["at", "in"]
-    words = word_context.split(" ")
-    word_index = words.index(word)
 
-    if word_index == 0:
+    previous_word = get_previous_word(word, word_context)
+
+    if not previous_word or previous_word == '':
         return 0
 
-    if words[word_index-1] in keywords:
+    if previous_word in keywords:
         return 1
 
     return 0
@@ -24,16 +60,23 @@ def has_keywords_before(word, word_context):
 def has_keywords_after(word, word_context):
     keywords = ["region", "square", "country", "city", "town", "county", "creek", "avenue", "court", "block", "street", "block", "drive",
                 "centre", "center", "ramp", "exit", "boulevard"]
-    words = word_context.split(" ")
-    word_index = words.index(word)
+    next_word = get_next_word(word, word_context)
 
-    if word_index == len(words)-1:
+    if not next_word or next_word == '':
         return 0
 
-    if words[word_index + 1].lower() in keywords:
+    if next_word.lower() in keywords:
         return 1
 
     return 0
+
+
+def contains_suffix(word):
+    if " " in word:
+        return 0
+    suffixes = ('land', 'berg', 'burg', 'shire', 'cester')
+
+    return 1 if word.endswith(suffixes) else 0
 
 
 def is_noun(word):
@@ -43,20 +86,21 @@ def is_noun(word):
 
 
 def is_location(word):
-    return 1 if "/LOCATION" in word else 0
+    result = re.search('^<LOCATION>(.*)</LOCATION>$', word)
+    return True if result else False
 
 
 def process_word(word):
-    return word.replace("/LOCATION", "")
+    # result = re.search('<LOCATION>(.*)</LOCATION>', word)
+    # return result.group(1)
+    return word.replace('<LOCATION>','').replace('</LOCATION>','')
 
 
 def get_feature_vector(rows):
     feature_vector = [[0] for x in range(len(rows))]
-    labels = []
 
     for i in range(len(rows)):
         word, word_context = rows[i][0], rows[i][1]
-        #word = process_word(crude_word)
         cur_vector = feature_vector[i]
 
         # cur_vector[0] = word
@@ -65,14 +109,14 @@ def get_feature_vector(rows):
         cur_vector.append(has_keywords_before(word, word_context))
         cur_vector.append(has_keywords_after(word, word_context))
         cur_vector.append(is_noun(word))
+        cur_vector.append(contains_suffix(word))
+        cur_vector.append(previous_word_direction(word, word_context))
+        cur_vector.append(is_all_caps(word))
 
-        labels.append(is_location(word))
-
-    return feature_vector, labels
+    return feature_vector
 
 
-def trainiing(rows, labels, v_rows, v_labels):
-    from sklearn.ensemble import RandomForestClassifier
+def trainiing(rows, labels, v_rows, v_labels, data):
     mean_scores = {}
     clf1 = RandomForestClassifier()
     # for row in rows:
@@ -86,20 +130,37 @@ def trainiing(rows, labels, v_rows, v_labels):
     # print "#" * 100
     # for predicted_label in predicted_labels:
     #     print predicted_label
+
+    print "###"*100
     correct=0
     for i in range(len(predicted_labels)):
-        if v_labels[i] == predicted_labels[i]:
+
+        if v_labels[i] == predicted_labels[i] and v_labels[i]==1:
+            print data[i][0]
             correct+=1
-    print correct
+
+    import collections
+    print collections.Counter(predicted_labels)
+    print collections.Counter(v_labels)
     print len(predicted_labels)
-    pdb.set_trace()
+    # pdb.set_trace()
     mean_scores["random-forest"] = scores.mean()
 
-    # from sklearn import tree
-    # clf2 = tree.DecisionTreeClassifier()
-    # clf2 = clf2.fit(training_set, training_set_labels)
-    # scores = cross_val_score(clf2, training_set, training_set_labels)
-    # mean_scores["decision-trees"] = scores.mean()
+
+    from sklearn import tree
+    clf2 = tree.DecisionTreeClassifier()
+    clf2 = clf2.fit(rows, labels)
+    scores = cross_val_score(clf2, rows, labels)
+    mean_scores["decision-trees"] = scores.mean()
+    predicted_labels = clf1.predict(v_rows)
+    for i in range(len(predicted_labels)):
+
+        if v_labels[i] == predicted_labels[i] and v_labels[i]==1:
+            print data[i][0]
+            correct+=1
+    print collections.Counter(predicted_labels)
+    print collections.Counter(v_labels)
+    print len(predicted_labels)
     #
     # from sklearn import linear_model
     # clf3 = linear_model.LinearRegression()
@@ -118,86 +179,97 @@ def trainiing(rows, labels, v_rows, v_labels):
     # scores = cross_val_score(clf5, training_set, training_set_labels)
     # mean_scores["svm"] = scores.mean()
 
+
 def is_invalid(word):
-    invalid_list = [".", ",", ". ", "'", ""]
+    invalid_list = [".", ",", ". ", "'", "", " "]
     return word in invalid_list
+
+
+def get_row_and_label(word, line):
+    # processed_word = word
+    label = 0
+    if is_location(word):
+        print "$$$" + word
+
+        # processed_word = process_word(word)
+        #line = line.replace(word, processed_word)
+        label = 1
+    line = line.replace("</LOCATION>", "").replace("<LOCATION>", "")
+    processed_word = process_word(word)
+    row = [processed_word, line, label]
+    return row, label
+
+
+def get_rows_and_labels(files):
+    rows = []
+    labels = []
+
+    for fname in files:
+        if not fname.endswith(".txt"):
+            continue
+
+        with open("./mod/" + fname) as f:
+            print fname
+            s = f.read()
+
+        lines = s.split("\n")
+
+        for line in lines:
+            words = line.split(" ")
+            num_words = len(words)
+            for i in range(num_words):
+                word = words[i]
+                if is_invalid(word):
+                    continue
+
+                row, label = get_row_and_label(word, line)
+                rows.append(row)
+                labels.append(label)
+
+                if i < num_words:
+                    next_word = words[i+1]
+                    if is_invalid(next_word):
+                        continue
+                    row, label = get_row_and_label(word + " " + next_word , line)
+                    rows.append(row)
+                    labels.append(label)
+
+    return rows, labels
+
 
 """
 Millions of people were left homeless in Indonesia#L Aceh#L 's region following the earthquake and tsunami disaster in late December .
 """
 def main():
-    rows = [
-        ["Millions", "Millions of people were left homeless in Indonesia#L Aceh#L region"],
-        ["of", "Millions of people were left homeless in Indonesia#L Aceh#L region"],
-        ["people", "Millions of people were left homeless in Indonesia#L Aceh#L region"],
-        ["were", "Millions of people were left homeless in Indonesia#L Aceh#L region"],
-        ["left", "Millions of people were left homeless in Indonesia#L Aceh#L region"],
-        ["homeless", "Millions of people were left homeless in Indonesia#L Aceh#L region"],
-        ["in", "Millions of people were left homeless in Indonesia#L Aceh#L region"],
-        ["Indonesia#L", "Millions of people were left homeless in Indonesia#L Aceh#L region"],
-        ["Aceh#L", "Millions of people were left homeless in Indonesia#L Aceh#L region"],
-        ["region", "Millions of people were left homeless in Indonesia#L Aceh#L region"],
-
-    ]
-    import os
-    rows = []
-    files = os.listdir("/Users/cyn0/cs839/stanford/stanford-ner-2017-06-09/mod/")
+    files = os.listdir("./mod/")
     # f = []
-    for fname in files[:400]:
-        if not fname.endswith(".txt"):
-            continue
-        with open("/Users/cyn0/cs839/stanford/stanford-ner-2017-06-09/mod/" + fname) as f:
-            print fname
-            s = f.read()
-        lines = s.split("\n")
-        for line in lines:
-            words = line.split(" ")
-            for word in words:
-                if is_invalid(word):
-                    continue
+    train_rows, train_labels = get_rows_and_labels(files[:40])
 
-                row = [word, line]
-                rows.append(row)
-
-            # for i in range(len(words)-1):
-            #     word = words[i]
-            #     if is_invalid(word):
-            #         continue
-            #     row = [word + " " + words[i+1], line]
-            #     rows.append(row)
-
-    validation_rows = []
-    for fname in files[400:]:
-        if not fname.endswith(".txt"):
-            continue
-        with open("/Users/cyn0/cs839/stanford/stanford-ner-2017-06-09/mod/" + fname) as f:
-            print fname
-            s = f.read()
-        lines = s.split("\n")
-        for line in lines:
-            words = line.split(" ")
-            for word in words:
-                if is_invalid(word):
-                    continue
-
-                row = [word, line]
-                validation_rows.append(row)
-
-            # for i in range(len(words)-1):
-            #     word = words[i]
-            #     if is_invalid(word):
-            #         continue
-            #     row = [word + " " + words[i+1], line]
-            #     validation_rows.append(row)
-    # for row in rows:
+    # for row in train_rows:
     #     print row
-    feature_vector, labels = get_feature_vector(rows)
-    print "#####" + str(labels.count(1))
-    validation_feature_vector, validation_labels = get_feature_vector(validation_rows)
+
+    validation_rows, validation_labels = get_rows_and_labels(files[40:51])
+
+    # for row in train_rows:
+    #     print row
+
+    feature_vector = get_feature_vector(train_rows)
+    # print "#####" + str(labels.count(1))
+    validation_feature_vector = get_feature_vector(validation_rows)
     #
+    print "############# training rows"
+    for i in range(len(train_rows)):
+        if train_labels[i] == 1:
+            print train_rows[i][0] + "#######" + train_rows[i][1]
+
+    print "#############validation rows"
+    for i in range(len(validation_rows)):
+        if validation_labels[i] == 1:
+            print validation_rows[i][0]
+
     # print len(feature_vector)
     # print "training"
-    trainiing(feature_vector, labels, validation_feature_vector, validation_labels)
+    trainiing(feature_vector, train_labels, validation_feature_vector, validation_labels, validation_rows)
     # for v in feature_vector:
     #     print v
 
